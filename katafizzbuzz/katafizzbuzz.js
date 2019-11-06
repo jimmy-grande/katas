445c67d87d34e4cbf9f4381d1ccad2f733866a8d
Jimmy Grande <jgrande@vente-privee.com>
2019-11-06 01:41:32 +0100

    handle case when param is multiple of 5

--- katafizzbuzz/katafizzbuzz.js
+++ katafizzbuzz/katafizzbuzz.js
@@ -2,6 +2,9 @@ function fizzbuzz(n) {
   if (n % 3 === 0) {
     return "Fizz";
   }
+  if (n % 5 === 0) {
+    return "Buzz";
+  }
   return n;
 }
 
--- katafizzbuzz/katafizzbuzz.spec.js
+++ katafizzbuzz/katafizzbuzz.spec.js
@@ -10,11 +10,22 @@ describe("Basic test", () => {
 });
 
 describe("Handle multiple of 3", () => {
-  it("should return Fizz when param a multiple of 3", () => {
+  it("should return Fizz when param is a multiple of 3", () => {
     expect(fizzbuzz(3)).toEqual("Fizz");
     expect(fizzbuzz(6)).toEqual("Fizz");
     expect(fizzbuzz(9)).toEqual("Fizz");
     expect(fizzbuzz(12)).toEqual("Fizz");
-    expect(fizzbuzz(15)).toEqual("Fizz");
+  });
+
+  it("should return Fizz when param a is multiple of 3 and negative", () => {
+    expect(fizzbuzz(-3)).toEqual("Fizz");
+    expect(fizzbuzz(-9)).toEqual("Fizz");
+  });
+});
+
+describe("Handle multiple of 5", () => {
+  it("should return Buzz when param a multiple of 5", () => {
+    expect(fizzbuzz(5)).toEqual("Buzz");
+    expect(fizzbuzz(10)).toEqual("Buzz");
   });
 });
